02ab410ac7dce679fa1c6fba25abfa0d7d9c4dda
Laila A. Andersen <lail5198@edu.easj.dk>
2021-02-20 17:48:46 +0100

    Milestone 2: TextView, EditText and Button is created

--- app/src/main/java/com/example/cleanarchitecturedemo/MainActivity.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.cleanarchitecturedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void enterInput(View view) {
+        EditText inputText = (EditText)
+findViewById(R.id.inputText);
+        TextView outputView = (TextView)
+findViewById(R.id.outputView);
+
+        String input = inputText.getText().toString();
+        outputView.setText(input);
     }
 }
