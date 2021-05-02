d0b03030ed9df60d8df98e6c7c682d8b83b85ab7
laan71 <lail5198@edu.easj.dk>
2021-05-02 18:39:36 +0200

    Data storage is separated from View and is stored and maintained in a separate Model class. The model class is placed in its own "model" package and the View class is placed it´s own "view" package.

--- app/src/main/java/com/example/cleanarchitecturedemo/AndroidView.java
+++ app/src/main/java/com/example/cleanarchitecturedemo/AndroidView.java
@@ -6,21 +6,28 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class AndroidView extends AppCompatActivity {
+
+     Model model = new Model();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView outputView = (TextView) findViewById(R.id.outputView);
+        outputView.setText(model.getData());
+
+        EditText inputText = (EditText) findViewById(R.id.inputText);
+        inputText.setText(model.getData());
     }
 
     public void enterInput(View view) {
-        EditText inputText = (EditText)
-findViewById(R.id.inputText);
-        TextView outputView = (TextView)
-findViewById(R.id.outputView);
+        TextView outputView = (TextView) findViewById(R.id.outputView);
+        EditText inputText = (EditText) findViewById(R.id.inputText);
 
         String input = inputText.getText().toString();
+        model.setData(input);
         outputView.setText(input);
     }
 }
---
+++ app/src/main/java/com/example/cleanarchitecturedemo/Model.java
@@ -0,0 +1,14 @@
+package com.example.cleanarchitecturedemo;
+
+public class Model {
+
+    private String data = "Hello";
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
